d489727680e41fe422f620b0f594fe618ed51d76
diego76 <diego.terzano@gmail.com>
2012-10-12 14:31:12 -0400

    Custom action result to render bitmap based charts to view for non javascript browsers.

---
+++ src/Sigma.Web.Shared/ActionResults/ChartResults.cs
@@ -0,0 +1,42 @@
+﻿#region License
+//-----------------------------------------------------------------------
+// <copyright file="ChartResult.cs" company="Pi2 LLC">
+//     Copyright (c) Pi2 LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+#endregion
+
+#region Using Directives 
+using System.Web.Helpers;
+using System.Web.Mvc;
+#endregion 
+
+namespace Sigma.Web.ActionResults
+{
+    /// <summary>
+    /// Helper result to render an image based chart to view. 
+    /// This is used for non javascript capable browsers.
+    /// </summary>
+    public class ChartResult : ActionResult
+    {
+        private readonly Chart _chart;
+        private readonly string _imageType;
+
+        public ChartResult(Chart chart) : this(chart, "jpeg") { }
+
+        public ChartResult(Chart chart, string imageType)
+        {
+            this._chart = chart;
+            this._imageType = imageType;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var result = new FileContentResult(
+                  _chart.GetBytes(_imageType),
+                  "image/" + _imageType);
+
+            result.ExecuteResult(context);
+        }
+    }
+}
