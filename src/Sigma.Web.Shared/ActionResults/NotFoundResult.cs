b9357b826e2017a5e1ba5ad7cf41e89091216a16
diego76 <diego.terzano@gmail.com>
2012-10-19 15:01:26 -0400

    Added AjaxOnly MVC attribute and Not Found action result to web shared project.

---
+++ src/Sigma.Web.Shared/ActionResults/NotFoundResult.cs
@@ -0,0 +1,26 @@
+﻿#region License
+//-----------------------------------------------------------------------
+// <copyright file="NotFoundResult.cs" company="Pi2 LLC">
+//     Copyright (c) Pi2 LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+#endregion
+
+#region Using Directives
+using System.Web.Mvc;
+#endregion
+
+namespace Sigma.Web.ActionResults
+{
+    public class NotFoundResult : ActionResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.StatusCode = 404;
+
+            new ViewResult { ViewName = "NotFound" }
+                .ExecuteResult(context);
+        }
+
+    }
+}
---
+++ src/Sigma.Web.Shared/Filters/AjaxOnlyAttribute.cs
@@ -0,0 +1,31 @@
+﻿#region License
+//-----------------------------------------------------------------------
+// <copyright file="AjaxOnlyAttribute.cs" company="Pi2 LLC">
+//     Copyright (c) Pi2 LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+#endregion
+
+#region Using Directives 
+using Sigma.Web.ActionResults;
+using System.Web.Mvc;
+#endregion
+
+namespace Sigma.Web.Filters
+{
+    public class AjaxOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.Result = new NotFoundResult();
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+    }
+}
