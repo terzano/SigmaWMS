89313ee82721d8a8f8aef37885901b6a235a396a
diego76 <diego.terzano@gmail.com>
2012-12-18 02:26:50 -0500

    Updated ServiceStack to 3.9.32. Updated NodaTime 1.0.1. Updated NConsoler. Updated a variety of js libraries.

--- src/Sigma.Api/App_Start/AppHost.cs
+++ src/Sigma.Api/App_Start/AppHost.cs
@@ -19,6 +19,7 @@ using ServiceStack.ServiceInterface.Auth;
 using ServiceStack.ServiceInterface.ServiceModel;
 using ServiceStack.WebHost.Endpoints;
 using Autofac;
+using System.Reflection;
 #endregion
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Sigma.Api.App_Start.AppHost), "Start")]
@@ -27,7 +28,7 @@ namespace Sigma.Api.App_Start
 {
     public class AppHost : AppHostBase
     {
-        public AppHost() : base("SigmaWMS API", typeof(Todo).Assembly) { }
+        public AppHost() : base("SigmaWMS API", Assembly.GetExecutingAssembly()) { }
 
         public override void Configure(Funq.Container container)
         {
@@ -38,16 +39,16 @@ namespace Sigma.Api.App_Start
             var builder = new ContainerBuilder();
             //Now register all depedencies to your custom IoC container
             //...
-            builder.RegisterType<TodoRepository>();
+            //builder.RegisterType<TodoRepository>();
 
             //Register Autofac IoC container adapter, so ServiceStack can use it
             IContainerAdapter adapter = new AutofacIocAdapter(builder.Build());
             container.Adapter = adapter;
 
             //Configure User Defined REST Paths
-            Routes
-                .Add<Todo>("/todos")
-                .Add<Todo>("/todos/{Id}");
+            //Routes
+            //    .Add<Todo>("/todos")
+            //    .Add<Todo>("/todos/{Id}");
         }
 
         public static void Start()
